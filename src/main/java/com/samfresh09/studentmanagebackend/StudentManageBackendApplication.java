ec8836c3c2bcbfc29e0f937a82092b6365f43ac4
samfresh09 <samtou.gnazouyoufei@ipnetinstitute.com>
2024-08-14 08:44:28 +0000

    initialisation du projet

---
+++ src/main/java/com/samfresh09/studentmanagebackend/StudentManageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.samfresh09.studentmanagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManageBackendApplication.class, args);
+	}
+
+}
